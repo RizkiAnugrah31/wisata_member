26f30d505d9b386d333ef8bda3bff17b0e018200
Nadya <nadya@codelabs.co.id>
2021-09-28 15:28:32 +0700

    tests

--- app/CredentialsModel.php
+++ app/CredentialsModel.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class CredentialsModel extends Model
+class CredentialsModels extends Model
 {
     //    Soft Delete
     use softDeletes;
