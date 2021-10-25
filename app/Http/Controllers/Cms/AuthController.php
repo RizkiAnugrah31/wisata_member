f99c126b100e5b9d45495fab53769ab4f65b8151
Nadya <nadya@codelabs.co.id>
2021-10-25 15:48:19 +0700

    25/10/2021

--- app/Http/Controllers/Cms/AuthController.php
+++ app/Http/Controllers/Cms/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Cms;
 
+use App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -12,19 +13,23 @@ class AuthController extends Controller
 {
     public function register (Request $request)
     {
-        $user_roles_id = $request->input('Id');
-        $employee_firstname = $request->input('firstname');
-        $employee_middlename = $request->input('middlename');
-        $employee_lastname = $request->input('lastname');
-        $employee_username= $request->input('username');
-        $employee_password = Hash::make($request->input('password'));
-        $employee_email = $request->input('email');
-        $employee_status = $request->input('status');
-        $employee_image = $request->input('image');
+        $data = $request->all();
+        $employee_id = $data["employee_id"]->toString();
+        $user_roles_id = $request->input('user_roles_id');
+        $employee_firstname = $request->input('employee_firstname');
+        $employee_middlename = $request->input('employee_middlename');
+        $employee_lastname = $request->input('employee_lastname');
+        $employee_username= $request->input('employee_username');
+        $employee_password = Hash::make($request->input('employee_password'));
+        $employee_email = $request->input('employee_email');
+        $employee_status = $request->input('employee_status');
+        $employee_image = $request->input('employee_image');
         $created_by = $request->input('created_by');
         $update_by = $request->input('update_by');
 
+    
         $register = EmployeeModel::create([
+        'employee_id' => $employee_id,
         'user_roles_id' => $user_roles_id,
         'employee_firstname' => $employee_firstname,
         'employee_middlename' => $employee_middlename,
