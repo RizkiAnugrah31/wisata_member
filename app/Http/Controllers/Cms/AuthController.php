f84be4235acd65393398dc7641532273336b4db8
Nadya <nadya@codelabs.co.id>
2021-10-27 17:02:27 +0700

    update 27/10/2021

--- app/Http/Controllers/Cms/AuthController.php
+++ app/Http/Controllers/Cms/AuthController.php
@@ -7,20 +7,27 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\EmployeeModel;
 use Firebase\JWT\JWT;
-use Illuminate\Validation\Validator;
+use Illuminate\Validation\Validate;
 
 class AuthController extends Controller
 {
     public function login (Request $request)
     {
         $validated = $this->validate($request, [
-            'employee_email' => $employee_email,
-            'employee_password' => $employee_password
+            'employee_email' => 'required|email|max:255|unique:employee,employee_email',
+            'employee_password' => 'required|exists:employee_password'
         ]);
+        
+        // $EmployeeModel = new User();
+        // $EmployeeModel->employee_email= $validated['employee_email'];
+        // $EmployeeModel->employee_password = Hash::make($validated['employee_password']);
+        // $EmployeeModel->save();
+        // return response()->json($EmployeeModel,201);
 
-        $EmployeeModel = EmployeeModel::where('employee_email', $validated['employee_email'])->first();
-        if (!Hash::make($validated['employee_password'], $EmployeeModel->employee_password)) {
+        $EmployeeModel = EmployeeModel::where('employee_email', $employee_email)->insert();
+        if (!Hash::check($EmployeeModel->employee_password)) {
             return abort (401, "email or password not valid");
+            return response()->json();
             
         }
         $payload = [
